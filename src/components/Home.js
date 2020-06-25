80cda8b3a729072e75048ced604088862158bc6c
seid zerihun <seidoo99@gmial.com>
2020-06-25 19:45:38 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         recipe: [
+       {name: 'spahgetti'
+        
+
+       }
+
+         ]
+      };
+    };
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
