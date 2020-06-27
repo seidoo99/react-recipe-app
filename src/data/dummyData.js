397aa62117c03d4560d5cf79e8f925cc732e642d
seid zerihun <seidoo99@gmial.com>
2020-06-27 12:25:13 -0400

    second commit

---
+++ src/data/dummyData.js
@@ -0,0 +1,30 @@
+var rawRecipes = [
+    {
+      name: 'Pumpkin Pie',
+      ingredients: [
+        'Pumpkin Puree',
+        'Sweetened Condensed Milk',
+        'Eggs',
+        'Pumpkin Pie Spice',
+        'Pie Crust'
+      ]
+    },
+    {
+      name: 'Spaghetti',
+      ingredients: [
+        'Noodles',
+        'Tomato Sauce',
+        '(Optional) Meatballs'
+      ]
+    },
+    {
+      name: 'Onion Pie',
+      ingredients: [
+        'Onion',
+        'Pie Crust',
+        'Sounds Yummy right?'
+      ]
+    }
+  ];
+  
+  export default rawRecipes;
